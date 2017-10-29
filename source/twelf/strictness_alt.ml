9e821fabc085e6f7bacf7f6d68c1bae57ecfc696
anwu@davidson.edu <anwu@davidson.edu>
2017-10-29 02:52:36 +0100

    fix some bugs

--- source/twelf/strictness_alt.ml
+++ source/twelf/strictness_alt.ml
@@ -50,11 +50,19 @@ end
 module IdSet = Set.Make(OrderedId)
 type idset = IdSet.t
 
-(*convert a set to a list *)
-let tolist = fun s -> IdSet.fold (fun x l -> x::l) s [];;        
+       
 type idMap = (id, idset) Hashtbl.t
+
+let setcopy set = IdSet.fold (fun x s -> IdSet.add x s) set IdSet.empty;;
+let mapcopy map = Hashtbl.fold (fun k v m -> Hashtbl.add m k (setcopy v)) map (Hashtbl.create 16);; 
+
+  
+(*convert a set to a list *)
+let tolist = fun s -> IdSet.fold (fun x l -> x::l) s [];; 
 (*convert a map to a list of pairs*) 
 let topairlist = fun h -> Hashtbl.fold (fun k v acc -> (k, (tolist v)) :: acc) h [];;
+
+
 
 type dependency = idMap (* id -> P(id), if x -> {y}, then if x is strict, then y is strict*)
 type delta = idset (* bounded variables in a type *)
@@ -64,7 +72,7 @@ and aneganntype = Neg of (id * aposanntype) list * id * term list * idset
 
            
 
-(* find all strict variables in type tp and annotate tp given a context gamma*)
+(* annotate tp given a context gamma*)
 let rec find_strict_vars_pos tp g =
   let (s, dep, ann_pairs, c, tms) = find_strict_vars_pos_rec tp g in
   	let s_final = finalize s dep in
@@ -80,7 +88,7 @@ and find_strict_vars_neg tp g =
         given positive type and context g *)
 and find_strict_vars_pos_rec tp g =
   match tp with
-    PiType (x, tpA, tpB) -> let (ann_tpA, sA) = find_strict_vars_neg tpA g
+    PiType (x, tpA, tpB) -> let (ann_tpA, sA) = find_strict_vars_neg tpA (setcopy g)
                           in let (s, dep, ann_pairs, tc, tms) =  find_strict_vars_pos_rec tpB  (IdSet.add x g)
                              in (s, (add_dep dep x (IdSet.union sA g)), (x, ann_tpA)::ann_pairs, tc, tms)
   | AppType (c, tms) -> ((union_fsvo_terms tms g), Hashtbl.create 16, [], c, tms)
@@ -91,8 +99,8 @@ and find_strict_vars_pos_rec tp g =
         given negative type and context g *)
 and find_strict_vars_neg_rec tp g =
   match tp with
-  | PiType (x, tpA, tpB) -> let (ann_tpA, sA) = find_strict_vars_pos tpA g
-                            in let (s, dep, ann_pairs, tc, tms) =  find_strict_vars_neg_rec tpB  (IdSet.add x g)
+  | PiType (x, tpA, tpB) -> let (ann_tpA, sA) = find_strict_vars_pos tpA (setcopy g)
+                            in let (s, dep, ann_pairs, tc, tms) =  find_strict_vars_neg_rec tpB  (IdSet.add x setcopy g)
                                in (s, (add_dep dep x (IdSet.union sA g)), (x, ann_tpA)::ann_pairs, tc, tms)
   | AppType (c, tms) -> ((union_fsvo_terms tms g), Hashtbl.create 16, [], c, tms)
   | ImpType (tpA, tpB) -> find_strict_vars_neg_rec tpB g                         
@@ -103,7 +111,7 @@ and find_strict_vars_neg_rec tp g =
 and union_fsvo_terms tms g =
   match tms with
   | [] -> IdSet.empty
-  | tm :: tms' -> IdSet.union (find_strict_vars_term tm g IdSet.empty) (union_fsvo_terms tms' g)
+  | tm :: tms' -> IdSet.union (find_strict_vars_term tm (setcopy g) IdSet.empty) (union_fsvo_terms tms' g)
 
                 
 (* add x to (dep[v] : list) for each v in l *)
@@ -129,12 +137,12 @@ and find_strict_vars_term tm g d =
 and all_ids_are_strict tms d checked : bool =
   match tms with
   | [] -> true
-  | (IdTerm v)::tms' -> IdSet.mem v d || not (IdSet.mem v checked) || all_ids_are_strict tms' d (IdSet.add v checked)
+  | (IdTerm v)::tms' -> IdSet.mem v d && not (IdSet.mem v checked) && all_ids_are_strict tms' d (IdSet.add v checked)
   | _ -> false
 
 (* the union of all strict variables found in tms *)
 and union_sv_subterms tms g d =
-  List.fold_left (fun vars tm -> IdSet.union (find_strict_vars_term tm g d) vars) IdSet.empty tms
+  List.fold_left (fun vars tm -> IdSet.union (find_strict_vars_term tm g (setcopy d)) vars) IdSet.empty tms
 
 (*finalize the set of strict variables with dependency information*)
 and finalize s dep =
@@ -145,3 +153,34 @@ and finalize s dep =
   in if s' = s then s'
      else finalize s' dep;;
        
+
+(* test case *)
+let idtp1 = Const "tp1";;
+let idtp2 = Const "tp2";;
+let idtp3 = Const "tp3";;
+
+let tp1 = IdType idtp1;;
+let tp2 = IdType idtp2;;
+let tp3 = IdType idtp3;;
+
+let v1 = Var ("v1", tp1);;
+let v2 = Var ("v2", tp2);;
+let v3 = Var ("v3", tp2);;
+
+let tm1 = AbsTerm (v3 , tp2 , AppTerm (v1, [IdTerm v3]));;
+let tm2 = IdTerm v2;;
+
+let tycon = Const "tycon";;
+
+let tmlist = [tm1];;
+
+let testtp = PiType (v1, tp1, (PiType (v2, tp2, AppType (tycon, tmlist))));;
+
+(* [v1].[v2]. c ({v3}.v1 v3)(v2) *)
+let test = find_strict_vars_neg testtp IdSet.empty;;
+
+let print_neg_ann_type tp =
+  match tp with
+    Neg (a, b, c, s) -> (a, b, c, tolist s);;
+
+print_neg_ann_type (fst test);;
