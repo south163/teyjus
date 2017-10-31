ab6f8eda08a816190df5623472736f745355ccd3
anwu@davidson.edu <anwu@davidson.edu>
2017-10-31 22:04:39 +0100

    change find_opt to find

--- source/twelf/strictness_alt.mli
+++ source/twelf/strictness_alt.mli
@@ -1,19 +1,7 @@
 (** Interface for Strictness Check **)
-val compare_symb : Symb.symbol -> Symb.symbol -> int
-module OrderedSymb : sig
-  type t
-  val compare : t -> t -> int
-  end
-                     
-module SymbSet = Set.S
 
-type symbset = SymbSet.t
-type symbMap = (Symb.symbol, symbset) Hashtbl.t
-
-(*Definition of types involved in the strictness functions*)
-type dependency = symbMap (* id -> P(id), if x -> {y}, then if x is strict, then y is strict*)
-type delta = symbset (* bounded variables in a term *)
-type gamma = symbset (* context *)
+type symbset
+type symbMap
 
 type aposanntype = Pos of (Symb.symbol * aneganntype) list * Lfabsyn.id * Lfabsyn.term list
 and aneganntype = Neg of (Symb.symbol * aposanntype) list * Lfabsyn.id * Lfabsyn.term list * symbset
